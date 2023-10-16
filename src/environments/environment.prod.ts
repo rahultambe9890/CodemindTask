7419ada610cbd6b6ac9539ac038959b173e52434
rahultambe9890 <rahultambe9890@gmail.com>
2023-10-16 15:39:02 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA2KEci_vl6JASc_eeHAIy-Lvzz5tPFqDU",
+    authDomain: "angular2023-efaf9.firebaseapp.com",
+    databaseURL: "https://angular2023-efaf9-default-rtdb.firebaseio.com",
+    projectId: "angular2023-efaf9",
+    storageBucket: "angular2023-efaf9.appspot.com",
+    messagingSenderId: "162647851753",
+    appId: "1:162647851753:web:3b75f584987ac860d76678",
+    measurementId: "G-T94LVD8EY2"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebaseConfig : {
+  apiKey: "AIzaSyA2KEci_vl6JASc_eeHAIy-Lvzz5tPFqDU",
+  authDomain: "angular2023-efaf9.firebaseapp.com",
+  databaseURL: "https://angular2023-efaf9-default-rtdb.firebaseio.com",
+  projectId: "angular2023-efaf9",
+  storageBucket: "angular2023-efaf9.appspot.com",
+  messagingSenderId: "162647851753",
+  appId: "1:162647851753:web:3b75f584987ac860d76678",
+  measurementId: "G-T94LVD8EY2"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
